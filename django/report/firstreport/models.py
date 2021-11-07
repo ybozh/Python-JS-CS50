8f1b0773e27f20ed2087d8433aba17b5a772b54b
Yuriy <to.yuriy@gmail.com>
2021-11-07 21:14:54 +0200

    ver 1

--- django/report/firstreport/models.py
+++ django/report/firstreport/models.py
@@ -1,19 +1,30 @@
 from django.db import models
-import datetime
+# import datetime
+from datetime import datetime
 
 # Create your models here.
+
+
 class Report(models.Model):
     city = models.CharField(max_length=64)
     customername = models.CharField(max_length=64)
     startdate = models.DateField()
     finishdate = models.DateField()
-    # duration = models.IntegerField(default=duration_def(self))
-    @property
-    def duration_def(self):
-        a = self.finishdate - self.startdate + datetime.timedelta(days=1)
-        return a.days
+    duration = models.IntegerField()
+    
+    # duration = models.DurationField()
+    # @property
+    # def duration_def(self):
+    #     a = self.finishdate - self.startdate + datetime.timedelta(days=1)
+    #     return a.days
 
-    
-    
+    def save(self, *args, **kwargs):
+        """
+        Should set duration field to total time between out and in data.
+        """
+        a = self.finishdate - self.startdate
+        self.duration = a.days + 1
+        super(Report, self).save(*args, **kwargs)
+
     def __str__(self):
         return f"{self.city} - {self.customername} - {self.startdate} - {self.finishdate}"
